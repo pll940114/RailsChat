d7398b58ff067c7f4a79e4168a71906e2c80ea25
linux40 <root@iZ2zeav8pzsweiez4qo3dmZ>
2018-12-23 20:08:13 +0800

    communicate with tuling robot

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,7 @@
+require 'json'
+require 'net/http'
+require 'monitor'
+
 class MessagesController < ApplicationController
   include SessionsHelper
   before_action :set_message, only: [:update, :destroy]
@@ -13,10 +17,10 @@ class MessagesController < ApplicationController
       redirect_to chat_path(chat), flash: {:warning => '消息发送失败'} and return
     end
     if "robot".eql? chat.users[-1].name and
-      send_to_robot chat.users[-1], message_params['body'], chat
+      send_to_robot chat.users[-1], message_params['body'], chat, -2
       redirect_to chat_path(chat), flash: {:warning => '回复消息失败'} and return
     elsif "robot".eql? chat.users[-2].name and
-         send_to_robot chat.users[-2], message_params['body'], chat
+         send_to_robot chat.users[-2], message_params['body'], chat, -1
       redirect_to chat_path(chat), flash: {:warning => '回复消息失败'} and return
     end
     redirect_to chat_path(chat)
@@ -45,8 +49,85 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:body)
   end
 
-  def send_to_robot (robot, message_body, chat)
-    reply_params = { 'body' => 'Hello world!' }
+  @@apikey_users = {
+    '51575c6f13db42a8af42fe4f2215a905' => [],
+    '6da94bb802f540af95091594a31d76f9' => [],
+    'd7021112324046fda711ae92c7660c65' => [],
+    'b2ad6c9c9a694d41ba71ad5fdf39ee31' => [],
+    '0c481d6075fb4c08a634c1fdf023c59b' => [],
+  }
+
+  @@user_array = nil
+
+  @@apikey_users_lock = Monitor.new
+
+  def self.allcate_apikey(id)
+    @@apikey_users.each do |key, array|
+      return key if array.include? id
+    end
+    @@apikey_users_lock.synchronize do
+      MessagesController.allcate_apikey_aux(id)
+    end
+  end
+
+  def self.allcate_apikey_aux(id)
+    first_key = nil
+    first_array = nil
+    next_one = false
+    @@apikey_users.each do |key, array|
+      if next_one
+        @@user_array = array
+        array << id
+        return key
+      end
+      if first_key.nil?
+        first_key = key
+        first_array = array
+      end
+      if @@user_array.equal? array
+        next_one = true
+      end
+    end
+    @@user_array = first_array
+    first_array << id
+    first_key
+  end
+
+  def send_to_robot(robot, message_body, chat, who)
+    hash = {
+      reqType: 0,
+      perception: { inputText: { text: message_body } },
+      userInfo: {
+        apiKey: MessagesController.allcate_apikey(chat.users[who].id),
+        userId: chat.users[who].id
+      }
+    }
+    data = MessagesController.capture_stdout { print(JSON.generate hash) }
+    uri = URI 'http://openapi.tuling123.com/openapi/api/v2'
+    http = Net::HTTP.new uri.host
+    response = http.post uri.path, data, { 'content-type' => 'text/plain', 'charset' => 'utf-8' }
+    case response
+    when Net::HTTPSuccess, Net::HTTPRedirection
+      puts '!!!!!!!'
+      puts response.body
+      reply_hash = JSON.parse response.body
+      if reply_hash["results"].nil?
+        return false
+      end
+      reply_hash["results"].each do |hash|
+        hash["values"].each do |key, value|
+          message = MessagesController.capture_stdout { print value }
+          return true if build_reply(robot, chat, message)
+        end
+      end
+      return false
+    else
+      return true
+    end
+  end
+
+  def build_reply(robot, chat, message)
+    reply_params = { 'body' => message }
     reply = robot.messages.build reply_params
     reply.chat = chat
     if reply.save
@@ -55,6 +136,15 @@ class MessagesController < ApplicationController
       return true
     end
     return false
+  end
+
+  def self.capture_stdout
+    origin = $stdout
+    $stdout = StringIO.new
+    yield
+    $stdout.string
+  ensure
+    $stdout = origin
   end
 end
 
